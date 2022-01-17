7083c17b4880fbd4e6df7836a557e8e8eb08bab5
Ariussssss <arius@qq.com>
2022-01-18 00:20:28 +0800

    :dizzy: Update at 2022-01-17

--- tricks/daily-info.sh
+++ tricks/daily-info.sh
@@ -2,7 +2,7 @@ BLUE='\033[38;5;45m'
 NC='\033[0m' # No Color
 
 # open "https://github.com/halfrost/Halfrost-Field"
-open "https://github.com"
+# open "https://github.com"
 open "https://github.com/trending?since=daily"
 open "https://news.ycombinator.com/newest"
 # open "https://blog.acolyer.org/"
@@ -11,10 +11,9 @@ open "https://docschina.org/weekly/react/docs"
 # open "https://github.com/jondot/awesome-weekly"
 open "https://github.com/"
 open "https://github.com/sorrycc/weekly"
-open "https://wubaiqing.github.io/zaobao/"
-open "https://github.com/ChanceYu/front-end-rss"
+# open "https://wubaiqing.github.io/zaobao/"
+# open "https://github.com/ChanceYu/front-end-rss"
 open "https://github.com/Advanced-Frontend/Daily-Interview-Question/issues"
-open "https://github.com/tnfe/TNT-Weekly"
 printf "👾 ${BLUE}Checkout daily trending.${NC}\n\n"
 
 total=$1
@@ -25,9 +24,9 @@ if [ $total -eq 1 -o $(date +%u) -eq 1 ]; then
     open "https://github.com/521xueweihan/HelloGitHub"
     open "https://github.com/dt-fe/weekly"
     open "https://github.com/ruanyf/weekly"
-    open "https://weekly.75.team/"
+    # open "https://weekly.75.team/"
     open "https://nodeweekly.com/issues"
     # open "https://cnodejs.org/?tab=share"
     printf "👾 ${BLUE}Checkout weekly.${NC}\n\n"
 fi  
-# open "https://github.com/hijiangtao/javascript-articles-monthly"
+open "https://github.com/hijiangtao/javascript-articles-monthly"
